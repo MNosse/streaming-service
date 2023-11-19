a965a1afc7598f29698c3d214a5c25fbc4834160
Mateus Nosse <80108925+MNosse@users.noreply.github.com>
2023-11-19 17:59:48 -0300

    Set up cors

--- src/main/kotlin/br/com/udesc/ceavi/eso/ese/streamingservice/StreamingServiceApplication.kt
+++ src/main/kotlin/br/com/udesc/ceavi/eso/ese/streamingservice/StreamingServiceApplication.kt
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class StreamingServiceApplication
@@ -24,5 +27,13 @@ class OpenApiConfig() {
 					.description("A Streaming web service")
 					.version("1.0")
 			)
+	}
+}
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry.addMapping("/**")
 	}
 }
